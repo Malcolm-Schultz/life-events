96387c8c6c06710cc96919d87f044e275df398d3
Brandon Montijo <mon0406@s286967.nml.com>
2018-06-19 16:17:25 -0500

    Changed color scheme of webApp and added survey page at the end of the leftNavBar list

--- src/js/navigation/index.js
+++ src/js/navigation/index.js
@@ -13,7 +13,7 @@ const showAndHideLowerNav = (pageIndex, numberOfPages) => {
 
 const highlightSideNav = (page, show) => {
   // show ? $(`#nav-${page.id}`).parent().addClass('active') : $(`#nav-${page.id}`).parent().removeClass('active')
-  show ? $(`#nav-${page.id}`).css({ 'background-color': '#16b161', color: 'white' }) : $(`#nav-${page.id}`).css({ 'background-color': 'transparent', color: '#9c9c9c' });
+  show ? $(`#nav-${page.id}`).css({ 'background-color': '#1678b1', color: 'white' }) : $(`#nav-${page.id}`).css({ 'background-color': 'transparent', color: '#9c9c9c' });
 };
 
 const updatePages = (pages, pageIndex) => (pages || []).map((page, i) => {
