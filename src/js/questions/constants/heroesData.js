3b832358eba371c8a87683d57a9a1cdfcbbf1d68
Bailey McElroy <mce8285@s286959.nml.com>
2018-06-20 11:38:29 -0500

    prelimanary question

---
+++ src/js/questions/constants/heroesData.js
@@ -0,0 +1,18 @@
+export const HEROES_DATA = [
+    {
+      id: 'monthly_net-income',
+      text: ''
+    },
+    {
+      id: '',
+      text: ''
+    },
+    {
+        id: ""
+
+    }
+  ];
+  
+  export default {
+    HOUSING_OPTIONS_DATA
+  };
